07016be9364da9534302a5ac90c3503a8a0efb47
sunmoon-idegu <sunmoon.idegu@gmail.com>
2026-01-20 19:43:35 +0800

    03: database setup using convex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Mono } from "next/font/google";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
-import { dark } from '@clerk/themes'
-import { ThemeProvider } from "@/components/theme-provider";
+
+import { Providers } from "@/components/providers";
 
 import "./globals.css";
 
@@ -36,34 +28,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{ theme: dark }}
-    >
-      <html lang="en" suppressHydrationWarning>
-        <body
-          className={`${inter.variable} ${plexMono.variable} antialiased`}
-        >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <header>
-              <SignedOut>
-                <SignInButton />
-                <SignUpButton>
-                  <button>Sign up</button>
-                </SignUpButton>
-              </SignedOut>
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </header>
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${inter.variable} ${plexMono.variable} antialiased`}
+      >
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
   );
 }
---
+++ src/components/providers.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { ClerkProvider, useAuth, UserButton } from "@clerk/nextjs";
+import { Authenticated, Unauthenticated, AuthLoading, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+import { ThemeProvider } from "./theme-provider";
+import { UnauthenticatedView } from "@/features/auth/components/unauthenticated-view";
+import { AuthLoadingView } from "@/features/auth/components/auth-loading-view";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <ClerkProvider>
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <Authenticated>
+                        <UserButton />
+                        {children}
+                    </Authenticated>
+                    <Unauthenticated>
+                        <UnauthenticatedView />
+                    </Unauthenticated>
+                    <AuthLoading>
+                        <AuthLoadingView />
+                    </AuthLoading>
+                </ThemeProvider>
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    )
+}
