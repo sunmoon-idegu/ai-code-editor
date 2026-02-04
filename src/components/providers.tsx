087717e1eb9e5e5c5856263dc4ba619669d202f0
sunmoon-idegu <sunmoon.idegu@gmail.com>
2026-02-04 12:40:02 +0800

    08: IDE layout

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import { ClerkProvider, useAuth, UserButton } from "@clerk/nextjs";
-import { Authenticated, Unauthenticated, AuthLoading, ConvexReactClient } from "convex/react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import {
+  Authenticated,
+  Unauthenticated,
+  AuthLoading,
+  ConvexReactClient,
+} from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 import { ThemeProvider } from "./theme-provider";
@@ -11,27 +16,26 @@ import { AuthLoadingView } from "@/features/auth/components/auth-loading-view";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <ClerkProvider>
-            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="dark"
-                    enableSystem
-                    disableTransitionOnChange
-                >
-                    <Authenticated>
-                        <UserButton />
-                        {children}
-                    </Authenticated>
-                    <Unauthenticated>
-                        <UnauthenticatedView />
-                    </Unauthenticated>
-                    <AuthLoading>
-                        <AuthLoadingView />
-                    </AuthLoading>
-                </ThemeProvider>
-            </ConvexProviderWithClerk>
-        </ClerkProvider>
-    )
-}
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Authenticated>
+            {children}
+          </Authenticated>
+          <Unauthenticated>
+            <UnauthenticatedView />
+          </Unauthenticated>
+          <AuthLoading>
+            <AuthLoadingView />
+          </AuthLoading>
+        </ThemeProvider>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
